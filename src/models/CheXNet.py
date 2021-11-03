28adc35650a599b9ab5809fc6a3818ff5e41e3e9
Raphael Attias <raphael.attias@outlook.com>
2021-11-03 23:09:38 +0100

    models

---
+++ src/models/CheXNet.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+"""
+The main CheXNet model implementation.
+"""
+
+
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+
+
+class DenseNet121(nn.Module):
+    """Model modified.
+
+    The architecture of our model is the same as standard DenseNet121
+    except the classifier layer which has an additional sigmoid function.
+
+    """
+    def __init__(self, out_size):
+        super(DenseNet121, self).__init__()
+        self.densenet121 = torchvision.models.densenet121(pretrained=True)
+        num_ftrs = self.densenet121.classifier.in_features
+        self.densenet121.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, out_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.densenet121(x)
+        return x
