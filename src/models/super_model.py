cb43863ea18db04598bdb7b171470e3095ba1397
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2021-12-17 13:39:28 +0100

    add style encoder

--- src/models/super_model.py
+++ src/models/super_model.py
@@ -1,5 +1,7 @@
 import torch
 from src.models.resnet import get_resnet
+from src.models.style_encoder import ConvEncoder
+
 
 class SuperModel(torch.nn.Module):
     def __init__(self, D_in, H, D_out):
@@ -9,7 +11,7 @@ class SuperModel(torch.nn.Module):
         """
         super(SuperModel, self).__init__()
         self.image_encoder = get_resnet(50, 2, 0.0625)
-        #self.style_encoder =  ???
+        self.style_encoder =  ConvEncoder()
         #self.linear1 = torch.nn.Linear(4096+style_encoder_out, l1)
         #self.linear2 = torch.nn.Linear(l1,l2)
         #self.linear3 = torch.nn.Linear(l2,l3)
